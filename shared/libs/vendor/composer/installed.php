2db262f88a64301381552000020bee1c3aad0fe0
Clément GAUCI <hello@clement-gauci.ovh>
2024-03-12 01:16:54 +0100

    Including shared libs

---
+++ shared/libs/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1e4281c25e4bd49b0446b7cc86d4199ec9f58972',
+        'name' => 'giulia/shared',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'giulia/shared' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1e4281c25e4bd49b0446b7cc86d4199ec9f58972',
+            'dev_requirement' => false,
+        ),
+    ),
+);
